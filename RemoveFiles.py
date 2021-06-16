61b9fc1532c262844dc9db194bfd18c28f8c0e92
jinal360 <73110780+jinal360@users.noreply.github.com>
2021-06-16 20:35:30 +0530

    Add files via upload

---
+++ RemoveFiles.py
@@ -0,0 +1,20 @@
+import time
+import os
+import shutil
+import os.path.exists
+import os.stat
+
+days = time.time()
+
+  os.path.exists(C:/Users/Jinal Mishra/Downloads)
+
+  os.walk(C:/Users/Jinal Mishra/Downloads):
+  os.path.join(sample1.txt ,sample2.txt)
+
+  ctime = os.stat(C:/Users/Jinal Mishra/Downloads).st_ctime
+
+  return ctime
+
+  os.remove(C:/Users/Jinal Mishra/Downloads)
+
+  print("message was not found")
